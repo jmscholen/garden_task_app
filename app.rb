6ee2eacd1d87f45064fef92c824b5baf146644c4
jmscholen <jmscholen@gmail.com>
2014-04-23 06:40:04 -0400

    updated to correct non-working application--too many changes to doc

--- app.rb
+++ app.rb
@@ -3,60 +3,64 @@ require "sinatra/activerecord"
 
 set :database, "sqlite3:///gardening_task_app.db"
 
-
+########THINGS TO ADD:
+######### - CSS CLASSES TO THE SHOW PAGE FOR EDIT AND DELETE
+#########
 get "/error" do   
-	erb :error 
+	erb :"to_dos/error"
 end
 
-get "Error" do 
-	erb :error 
+get "/Error" do 
+	erb :"to_dos/error" 
 end
-
+############Read Your List###########
 get "/" do
 	@to_dos = GardeningTask.all #@to_dos is an instance variable(array)
-	erb :main
+	erb :"to_dos/show"
+end
+
+############Post a new task which is pulled from :new Page
+get "/new" do
+	erb :"to_dos/new"
 end
 
 post "/to_dos" do
-	 @new_garden_task = GardeningTask.new #(params[:gardening_task])
-	 if @new_garden_task.accept_task("#{params[:description]}")
+	 @new_garden_task = GardeningTask.new(params[:gardening_task])
+	 @check_description = @new_garden_task.description
+	 if @new_garden_task.accept_task(@check_description)
 	 		@new_garden_task.save
 			redirect "/"
 		else
-			erb :error
+			erb :"to_dos/error"
 		end
 end
-
-put "/to_dos/:id" do
+##############Edit a task which is pulled from :edit Pag
+###########
+get "/to_dos/:id/edit" do
 	@to_dos = GardeningTask.find(params[:id])
-		if @to_dos.find_attributes(parama[:id])
-			erb :"to_dos/#{:id}"
-		else
-			erb :error_mistake
-		end
+	
+	erb :"/to_dos/edit"
 end
 
 put "/to_dos/:id/edit" do
 	@to_dos = GardeningTask.find(params[:id])
-	if @to_dos.find_attributes(parama[:id])
-			erb :"to_dos/#{:id}/edit"
-		else
-			erb :error_mistake
-		end
-end
-get "/choose" do
-	erb :choose
-end
-
-delete "/to_dos/:id/delete" do
-	@to_dos = GardeningTask.find(params[:id])
+	@to_dos.update_attributes(params[:gardening_tasks])
 	
-	erb :delete
+	redirect "/"
+
 end
 
-class	GardeningTask < ActiveRecord::Base
+
+get "/to_dos/:id/delete" do
+	@to_dos = GardeningTask.find(params[:id])
+	@to_dos.destroy
+
+	redirect "/"
+end
+
+class GardeningTask < ActiveRecord::Base
 	# @@tasks = ["Hedge the Bushes", "Trim the Poplar"]
-	# @@common_garden_tasks = ["prune", "cut", "hedge", "trim", "mow", "plant", "cultivate", "till"]
+	#@@common_garden_tasks = ["prune", "cut", "hedge", "trim", "mow", "plant", "cultivate", "till"]
 	
 	# def initialize (commenting out due to NoMethodError - undefined method `delete' for nil:NilClass:...this was suggested by someone on SOF)
 		
@@ -68,18 +72,19 @@ class	GardeningTask < ActiveRecord::Base
 	# end
 
 
-	def self.accept_task(text)
-		 if evaluate_task(text) == true
-
-				return true
-			else
-				return true # changed from false to test
-			end	
+	def accept_task(text)
+		 if evaluate_task(text) 
+		 	return true
+		else
+			return false
+		end	
 	end
 
-	def self.evaluate_task(text)
+	def evaluate_task(text)
 		task_to_split = text.downcase
+		print (task_to_split + "downcase")
 		split_task = task_to_split.split(" ")
+		print (split_task)
 		unmatched_tasks = (["prune", "cut", "hedge", "trim", "mow", "plant", "cultivate", "till"] - split_task)
 		if unmatched_tasks.length < ["prune", "cut", "hedge", "trim", "mow", "plant", "cultivate", "till"].length
 			return true
